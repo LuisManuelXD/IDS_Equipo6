a2dda446453755ebd38d2a4cb7ef5b2751631d90
riostorres <riostorresGamer@gmail.com>
2022-06-15 00:38:47 -0500

    Se le agrego el metodo resta, suma y multiplicacion.

--- calculadora.c
+++ calculadora.c
@@ -2,6 +2,7 @@
 
 float suma(float sumaNumero1, float sumaNumero2);
 float resta(float restaNumero1, float restaNumero2);
+float multiplicacion(float multiNumero1, float multiNumero2);
 
 int main() {
     float numero1, numero2;
@@ -25,11 +26,11 @@ int main() {
             printf("La resta de los numeros es: %.2f", resta(numero1, numero2));
         break;
 
-        /*case '3':
+        case '3':
             printf("La multiplicacion de los numeros es: %.2f", multiplicacion(numero1, numero2));
         break;
 
-        case '4':
+        /*case '4':
             printf("La division de los numeros es: %.2f", division(numero1, numero2));
         break;*/
 
@@ -46,4 +47,8 @@ float suma(float sumaNumero1, float sumaNumero2) {
 
 float resta(float restaNumero1, float restaNumero2) {
     return restaNumero1 - restaNumero2;
+}
+
+float multiplicacion(float multiNumero1, float multiNumero2) {
+    return multiNumero1 * multiNumero2;
 }
